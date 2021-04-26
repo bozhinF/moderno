3402bd975522d3c92b0e2ce843ef05dc23a045e3
bozhinF <bojin.fedor@gmail.com>
2021-04-26 21:24:38 +0300

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -14,6 +14,16 @@ $(function(){
         arrows: false,
     });
 
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
+
+    $('.header__btn-menu').on('click', function(){
+        $('.header__box').toggleClass('active');
+    });
+
+
+
     var mixer = mixitup('.products__inner-box');
 
     
